190e0cceb1ba1e4f820334f9ced3e48fbb5bde67
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,4 @@
+return {
+    ["General knowledge quiz — everyone writes their answer, then reveal."] = { fr = "Quiz de culture générale — chacun écrit sa réponse, puis on révèle." },
+    ["Quiz Party"] = { fr = "Quiz Party" },
+}
